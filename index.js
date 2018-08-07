3e7376270378f0ce51bfc5639d17fab2fbf1de1e
刘士 <liushilive@outlook.com>
2018-08-07 18:34:05 +0800

    1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Katex = require('./lib/Katex');
 var Mermaid = require('./lib/Mermaid');
 var Tools = require('./lib/Tools');
+var Prism = require('./lib/Prism');
+var ImageCaptions = require('./lib/ImageCaptions');
 
 
-module.exports = { Katex, Mermaid, Tools };
+module.exports = { Katex, Mermaid, Tools, Prism, ImageCaptions };

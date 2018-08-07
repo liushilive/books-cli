d860ba3613f8ebc97563ac28ddc4702069204712
刘士 <liushilive@outlook.com>
2018-08-07 13:06:20 +0800

    1

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-var katex = require("katex");
-var path = require('path');
-var cpr = require('cpr');
+var Katex = require('./lib/Katex');
+var Mermaid = require('./lib/Mermaid');
+var Tools = require('./lib/Tools');
 
-/**
- * 复制文件或文件夹
- * @param {string} source
- * @param {string} destination
- */
-function copy(source, destination) {
-  cpr(source, destination, {
-    deleteFirst: false, //Delete "to" before
-    overwrite: true, //If the file exists, overwrite it
-    confirm: true //After the copy, stat all the copied files to make sure they are there
-  }, function (err, files) {
-    //err - The error if any (err.list might be available with an array of errors for more detailed information)
-    if (err) {
-      throw console.error(err);
-    }
-    //files - List of files that we copied
-    console.debug('复制资源文件', files);
-  });
-}
 
-module.exports = { copy: copy, };
+module.exports = { Katex, Mermaid, Tools };

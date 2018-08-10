2f23e1d63f405128c36b9eed9ae9aa4d396c3ae0
刘士 <liushilive@outlook.com>
2018-08-10 20:02:44 +0800

    1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Tools = require('./lib/Tools');
 const Prism = require('./lib/Prism');
 const ImageCaptions = require('./lib/ImageCaptions');
 const sectionx = require('./lib/sectionx');
+const file_imports = require('./lib/file_imports');
 
 
-module.exports = { Katex, Mermaid, Tools, Prism, ImageCaptions, sectionx };
+module.exports = { Katex, Mermaid, Tools, Prism, ImageCaptions, sectionx, file_imports };

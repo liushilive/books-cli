b71c832cb7491e27bcf6ce1d963656ae7a2dfdee
刘士 <liushilive@outlook.com>
2018-08-14 10:56:36 +0800

    search

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const Prism = require('./lib/Prism');
 const ImageCaptions = require('./lib/ImageCaptions');
 const sectionx = require('./lib/sectionx');
 const file_imports = require('./lib/file_imports');
+const page_footer_copyright = require('./lib/page_footer_copyright');
+const search_plus = require('./lib/search_plus');
 
 
-module.exports = { Katex, Mermaid, Tools, Prism, ImageCaptions, sectionx, file_imports };
+module.exports = { Katex, Mermaid, Tools, Prism, ImageCaptions, sectionx, file_imports, page_footer_copyright: page_footer_copyright.page_footer_copyright, search_plus };
